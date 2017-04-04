3c1cd7eb8f75c3893077a272763df9610114ca39
aprietof <adrianprieto00@gmail.com>
2017-04-04 00:56:58 -0400

    Navbar search and about components added with routing

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'search',
+  templateUrl: 'search.component.html',
+  styleUrls: ['search.component.css']
+})
+
+export class SearchComponent { }
