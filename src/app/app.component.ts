21f41197b4d4e2a5e87acb72e497e83a6bede90f
aprietof <adrianprieto00@gmail.com>
2017-04-04 11:33:36 -0400

    spotify service added to search component just console logging results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { SearchComponent } from './components/search/search.component';
+import { HttpModule } from '@angular/http';
 
 @Component({
   moduleId: module.id,
@@ -12,7 +13,8 @@ import { SearchComponent } from './components/search/search.component';
     NavbarComponent, 
     AboutComponent, 
     SearchComponent
-  ]
+  ],
+  providers: [HttpModule]
 })
 
 export class AppComponent { }
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { SpotyfyService } from '../../services/spotify.service';
 
 @Component({
   moduleId: module.id,
   selector: 'search',
   templateUrl: 'search.component.html',
-  styleUrls: ['search.component.css']
+  styleUrls: ['search.component.css'],
+  providers: [SpotyfyService]
 })
 
-export class SearchComponent { }
+export class SearchComponent { 
+  searchStr: string;
+
+  constructor(private _spotifyService:SpotyfyService){
+
+  }
+
+  searchMusic() {
+    this._spotifyService.searchMusic(this.searchStr)
+      .subscribe(res => {
+        console.log(res.artists.items)
+      })
+  }
+}
