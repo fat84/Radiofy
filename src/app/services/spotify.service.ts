ebef04b4745d853a717c8191e097a4b7cfadde8b
aprietof <adrianprieto00@gmail.com>
2017-04-04 16:06:54 -0400

    list of albums added to artist component

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -7,20 +7,29 @@ import 'rxjs/add/operator/map';
 export class SpotyfyService {
   private searchUrl: string;
   private addressUrl: string;
+  private albumsUrl: string;
+
+  private spotifyAPI = 'https://api.spotify.com/v1/';
 
   constructor(private _http:Http){
 
   }
 
   searchMusic(str:string, type='artist'){
-    this.searchUrl = 'https://api.spotify.com/v1/search?query='+str+'&offset=0&limit=20&type='+type+'&market=US';
+    this.searchUrl = this.spotifyAPI + 'search?query=' + str + '&offset=0&limit=20&type=' + type + '&market=US';
     return this._http.get(this.searchUrl)
             .map(res => res.json());
   }
 
   getArtist(id:string){
-    this.addressUrl = 'https://api.spotify.com/v1/artists/'+id;
+    this.addressUrl = this.spotifyAPI+'artists/' + id;
     return this._http.get(this.addressUrl)
+            .map(res => res.json());
+  }
+
+  getAlbums(artistId:string){
+    this.albumsUrl = this.spotifyAPI + 'artists/' + artistId + '/albums';
+    return this._http.get(this.albumsUrl)
             .map(res => res.json());
   }
 }
